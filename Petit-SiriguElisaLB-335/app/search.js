811a562681c0fd2fc393be94bb1d2341d5328035
Elisa Petit-Sirigu <E.Petit.inf21@stud.bbbaden.ch>
2023-12-17 10:39:53 +0100

    finished search screen

--- Petit-SiriguElisaLB-335/app/search.js
+++ Petit-SiriguElisaLB-335/app/search.js
@@ -1,152 +1,93 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, TouchableWithoutFeedback, Keyboard, Text, TextInput, ScrollView, TouchableOpacity } from 'react-native';
-import { useStocksContext } from '../contexts/StocksContext';
-import { scaleSize } from '../constants/Layout';
-import { Ionicons } from '@expo/vector-icons';
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, TextInput, StyleSheet, Pressable, TouchableOpacity } from "react-native";
+import { Link, router } from "expo-router";
 
-function PromptText({ children }) {
-  return (
-    <Text style={styles.promptText}>{children}</Text>
-  )
-}
+const StockScreen = () => {
+  const [searchText, setSearchText] = useState("");
+  const [stocks, setStocks] = useState([]);
 
-function SearchBox({ searchText, handleChangeSearchText }) {
-  return (
-    <View style={styles.searchSection}>
-      <Ionicons style={styles.searchIcon} name="md-search" />
-      <TextInput
-        style={styles.searchInput}
-        placeholder="Search"
-        placeholderTextColor="#fff"
-        defaultValue={searchText}
-        onChangeText={(text) => handleChangeSearchText(text)}
-      />
-    </View>
-  )
-}
+  const fetchStockData = async () => {
+    try {
+      const response = await fetch(
+        "http://api.marketstack.com/v1/eod?access_key=dcfbf46b058adba28731fe0693a93f63&symbols=ALC,LOGN,CFR,ROG,AAPL,MSFT,AMZN,FB,KO,DIS,NFLX,NKE,TSLA,DDAIF,MSFT"
+      );
+      const { data } = await response.json();
 
-function StockListItem({ stock }) {
-  return (
-    <View style={styles.stockListItem}>
-      <Text style={styles.stockSymbol}>{stock.symbol}</Text>
-      <Text style={styles.stockName}>{stock.name}</Text>
-    </View>
-  )
-}
+      const uniqueStocks = Array.from(
+        new Set(data.map((stock) => stock.symbol))
+      ).map((symbol) => {
+        const stockData = data.find((stock) => stock.symbol === symbol);
+        return {
+          symbol: stockData.symbol,
+          high: stockData.high,
+          low: stockData.low,
+        };
+      });
 
-function StockList({ stocks, addStockToWatchList }) {
-  return (
-    <ScrollView>
-      {stocks.map(stock => (
-        <TouchableOpacity onPress={() => addStockToWatchList(stock)} key={stock.symbol}>
-          <StockListItem stock={stock} />
-        </TouchableOpacity>
-      ))}
-    </ScrollView>
-  )
-}
-
-export default function SearchScreen({ navigation }) {
-  const { ServerURL, addToWatchlist } = useStocksContext();
-
-  const [state, setState] = useState({
-    stocks: [], //all stocks
-    searchText: "",
-    filteredStocks: [], // stocks filtered by the "searchText"
-  });
+      setStocks(uniqueStocks);
+    } catch (error) {
+      console.error("Error fetching stock data:", error);
+    }
+  };
 
   useEffect(() => {
-    fetch(`${`http://api.marketstack.com/v1/eod?access_key=dcfbf46b058adba28731fe0693a93f63&symbols=ALC,LOGN,CFR,ROG,AAPL,MSFT,AMZN,FB,KO,DIS,NFLX,NKE,TSLA,DDAIF,MSFT`}/all`)
-      .then(res => res.json())
-      .then(stockData => { setState(prev => ({ ...prev, stocks: stockData })) })
-      .catch(err => console.error(err));
-  }, []); // [] empty, treated as componentDidMount
+    fetchStockData();
+  }, []);
 
-  const handleChangeSearchText = (text) => {
-    text = text.replace(/[.*+\-?^${}()|[\]\\]/g, '');
-    const regex = RegExp(text, "i");
-    setState(prev => ({
-      ...prev,
-      searchText: text,
-      filteredStocks: state.stocks.filter(stock => regex.test(stock.symbol) || regex.test(stock.name))
-    }))
-  }
+  const filteredStocks = stocks.filter((stock) =>
+    stock.symbol.toLowerCase().includes(searchText.toLowerCase())
+  );
 
-  const handleAddStockToWatchList = (stock) => {
-    addToWatchlist(stock.symbol);
-    navigation.navigate('Stocks');
-  }
+  const renderStockItem = ({ item }) => (
+    <TouchableOpacity onPress={() =>
+      router.push({
+        pathname: "/diagrams/[id]",
+        params: { id: Symbol },
+      })}>
+      <View style={styles.stockItem}>
+        <Text style={styles.stockSymbol}>{item.symbol}</Text>
+        <Text>High: {item.high}</Text>
+        <Text>Low: {item.low}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-        {/* FixMe: add children here! */}
-        <PromptText>Type a company name or stock symbol:</PromptText>
-
-        <SearchBox
-          searchText={state.searchText}
-          handleChangeSearchText={handleChangeSearchText}
-        />
-
-        {state.searchText !== "" &&
-          <StockList
-            stocks={state.filteredStocks}
-            addStockToWatchList={handleAddStockToWatchList}
-          />}
-
-      </View>
-    </TouchableWithoutFeedback>
-  )
-}
+    <View style={styles.container}>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search stock symbol"
+        value={searchText}
+        onChangeText={(text) => setSearchText(text)}
+      />
+      <FlatList
+        data={filteredStocks}
+        renderItem={renderStockItem}
+        keyExtractor={(item) => item.symbol}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
+    padding: 16,
   },
-
-  promptText: {
-    marginTop: scaleSize(5),
-    color: "#fff",
-    textAlign: "center",
-  },
-
-  searchSection: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#1F1F1F',
-    height: scaleSize(40),
-    borderRadius: scaleSize(10),
-    marginHorizontal: scaleSize(3),
-    marginTop: scaleSize(5),
-  },
-
-  searchIcon: {
-    paddingHorizontal: scaleSize(15),
-    color: '#fff',
-    fontSize: scaleSize(20),
-  },
-
   searchInput: {
-    flex: 1,
-    color: '#fff',
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 16,
+    padding: 8,
   },
-
-  stockListItem: {
-    paddingBottom: scaleSize(10),
-    borderBottomColor: "#2F2F2F",
-    borderBottomWidth: scaleSize(1),
+  stockItem: {
+    marginBottom: 16,
   },
-
   stockSymbol: {
-    paddingHorizontal: scaleSize(10),
-    paddingTop: scaleSize(10),
-    color: "#fff",
-    fontSize: scaleSize(20),
-  },
-
-  stockName: {
-    paddingHorizontal: scaleSize(10),
-    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
+
+export default StockScreen;
