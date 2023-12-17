71c6ba19aaae05a3be623daf425fbd6c84f68d3d
Elisa Petit-Sirigu <E.Petit.inf21@stud.bbbaden.ch>
2023-12-17 09:58:05 +0100

    finished API fetch

--- Petit-SiriguElisaLB-335/app/search.js
+++ Petit-SiriguElisaLB-335/app/search.js
@@ -1,84 +1,152 @@
-import { StyleSheet, Text, View } from "react-native";
-import { useEffect, useState } from "react";
-import { State, TextInput, ScrollView } from "react-native-gesture-handler";
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, TouchableWithoutFeedback, Keyboard, Text, TextInput, ScrollView, TouchableOpacity } from 'react-native';
+import { useStocksContext } from '../contexts/StocksContext';
+import { scaleSize } from '../constants/Layout';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function Page() {
+function PromptText({ children }) {
+  return (
+    <Text style={styles.promptText}>{children}</Text>
+  )
+}
+
+function SearchBox({ searchText, handleChangeSearchText }) {
+  return (
+    <View style={styles.searchSection}>
+      <Ionicons style={styles.searchIcon} name="md-search" />
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search"
+        placeholderTextColor="#fff"
+        defaultValue={searchText}
+        onChangeText={(text) => handleChangeSearchText(text)}
+      />
+    </View>
+  )
+}
+
+function StockListItem({ stock }) {
+  return (
+    <View style={styles.stockListItem}>
+      <Text style={styles.stockSymbol}>{stock.symbol}</Text>
+      <Text style={styles.stockName}>{stock.name}</Text>
+    </View>
+  )
+}
+
+function StockList({ stocks, addStockToWatchList }) {
+  return (
+    <ScrollView>
+      {stocks.map(stock => (
+        <TouchableOpacity onPress={() => addStockToWatchList(stock)} key={stock.symbol}>
+          <StockListItem stock={stock} />
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
+  )
+}
+
+export default function SearchScreen({ navigation }) {
+  const { ServerURL, addToWatchlist } = useStocksContext();
+
   const [state, setState] = useState({
+    stocks: [], //all stocks
     searchText: "",
-    stocks: [],
+    filteredStocks: [], // stocks filtered by the "searchText"
   });
 
   useEffect(() => {
-    fetch("http://api.marketstack.com/v1/eod?access_key=dcfbf46b058adba28731fe0693a93f63&symbols=ALC,LOGN,CFR,ROG,AAPL,MSFT,AMZN,FB,KO,DIS,NFLX,NKE,TSLA,DDAIF,MSFT")
+    fetch(`${`http://api.marketstack.com/v1/eod?access_key=dcfbf46b058adba28731fe0693a93f63&symbols=ALC,LOGN,CFR,ROG,AAPL,MSFT,AMZN,FB,KO,DIS,NFLX,NKE,TSLA,DDAIF,MSFT`}/all`)
       .then(res => res.json())
-      .then(stockData => setState(prev => ({ ...prev, stocks: stockData})))
+      .then(stockData => { setState(prev => ({ ...prev, stocks: stockData })) })
       .catch(err => console.error(err));
-  }, []);
+  }, []); // [] empty, treated as componentDidMount
+
+  const handleChangeSearchText = (text) => {
+    text = text.replace(/[.*+\-?^${}()|[\]\\]/g, '');
+    const regex = RegExp(text, "i");
+    setState(prev => ({
+      ...prev,
+      searchText: text,
+      filteredStocks: state.stocks.filter(stock => regex.test(stock.symbol) || regex.test(stock.name))
+    }))
+  }
+
+  const handleAddStockToWatchList = (stock) => {
+    addToWatchlist(stock.symbol);
+    navigation.navigate('Stocks');
+  }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.hintText}>Type a company name or stock symbol:</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        {/* FixMe: add children here! */}
+        <PromptText>Type a company name or stock symbol:</PromptText>
 
-      <View style={styles.searchSection}>
-        <TextInput
-          style={styles.serachInput}
-          placeholder="Search"
-          placeholderTextColor="#fff"
-          defaultValue={state.searchText}
-          onChangeText={(text) =>
-            setState((prev) => ({ ...prev, searchText: text }))
-          }
+        <SearchBox
+          searchText={state.searchText}
+          handleChangeSearchText={handleChangeSearchText}
         />
-      </View>
 
-      <ScrollView>
-        {state.stocks.map((stock) => (
-          <View key={stock.symbol}>
-            <Text style={styles.stockSymbol}>{stock.symbol}</Text>
-            <Text style={styles.stockName}>{stock.name}</Text>
-            <View style={styles.divider} />
-          </View>
-        ))}
-      </ScrollView>
-    </View>
-  );
+        {state.searchText !== "" &&
+          <StockList
+            stocks={state.filteredStocks}
+            addStockToWatchList={handleAddStockToWatchList}
+          />}
+
+      </View>
+    </TouchableWithoutFeedback>
+  )
 }
 
 const styles = StyleSheet.create({
-  stockSymbol: {
-    paddingHorizontal: 10,
-    paddingTop: 10,
-    color: "#fff",
-    fontSize: 20,
-  },
-  stockName: {
-    paddingHorizontal: 10,
-    color: "#fff",
-  },
-  divider: {
-    marginTop: 10,
-    borderBottomColor: "#2f2f2f",
-    borderBottomWidth: 1,
-  },
 
   container: {
     flex: 1,
-    padding: 10,
-    backgroundColor: "black",
   },
-  hintText: {
+
+  promptText: {
+    marginTop: scaleSize(5),
     color: "#fff",
     textAlign: "center",
-    marginVertical: 5,
   },
+
   searchSection: {
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#1f1f1f",
-    height: 40,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#1F1F1F',
+    height: scaleSize(40),
+    borderRadius: scaleSize(10),
+    marginHorizontal: scaleSize(3),
+    marginTop: scaleSize(5),
   },
-  serachInput: {
+
+  searchIcon: {
+    paddingHorizontal: scaleSize(15),
+    color: '#fff',
+    fontSize: scaleSize(20),
+  },
+
+  searchInput: {
     flex: 1,
+    color: '#fff',
+  },
+
+  stockListItem: {
+    paddingBottom: scaleSize(10),
+    borderBottomColor: "#2F2F2F",
+    borderBottomWidth: scaleSize(1),
+  },
+
+  stockSymbol: {
+    paddingHorizontal: scaleSize(10),
+    paddingTop: scaleSize(10),
+    color: "#fff",
+    fontSize: scaleSize(20),
+  },
+
+  stockName: {
+    paddingHorizontal: scaleSize(10),
     color: "#fff",
   },
 });
